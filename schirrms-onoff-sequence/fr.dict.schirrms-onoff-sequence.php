f46782b17e4a20a517267729412954b05e744b5f
Schirrms <48877593+Schirrms@users.noreply.github.com>
2022-02-23 13:54:45 +0100

    Initial Upload

---
+++ schirrms-onoff-sequence/fr.dict.schirrms-onoff-sequence.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Localized data
+ *
+ * @copyright   Copyright (C) 2013 XXXXX
+ * @license     http://opensource.org/licenses/AGPL-3.0
+ */
+
+Dict::Add('FR FR', 'French', 'Français', array(
+	// Dictionary entries go here
+	'Class:FunctionalCI/Attribute:sequencenumber' => 'Numéro de séquence',
+	'Class:FunctionalCI/Attribute:sequencenumber+' => 'Numéro de séquencement ON/OFF.',
+));
+?>
